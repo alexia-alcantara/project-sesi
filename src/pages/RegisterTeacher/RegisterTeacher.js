75160daeb18d7f01e5e91fbd9180094f5238aa83
alexia-alcantara <alexialcantara@outlook.com>
2025-01-12 20:25:05 -0300

    refactor: correção da validação do formulário

--- src/pages/RegisterTeacher/RegisterTeacher.js
+++ src/pages/RegisterTeacher/RegisterTeacher.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useForm, FieldValues } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { withMask } from 'use-mask-input';
 import './RegisterTeacher.css'
@@ -7,7 +7,7 @@ import './RegisterTeacher.css'
 const RegisterTeacher = () => {
     const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
-    const { handleSubmit, register, formState: { isSubmitting, errors}} = useForm();
+    const { handleSubmit, register, getValues, formState: { isSubmitting, errors}} = useForm();
     const onSubmit = data => console.log(data);
     console.log(errors);
 
@@ -37,36 +37,64 @@ const RegisterTeacher = () => {
        <p>Preencha os dados abaixo para criar sua conta.</p>
 
        <form onSubmit={handleSubmit(onSubmit)}>
-                <div className='form-group'>
-                    <label>Nome Completo</label>
-                    <input type="text" id="name" {...register('name', {
-                        required: 'Este campo é obrigatório'
-                    })}
-                    placeholder="Digite seu nome completo" 
-                     />
-                     {/* Sugestão de exibir erro de validação */}
-                     {/* <div className='min-h-4'>
-                        <p className='text-xs text-red-400 mt-1'> {errors.name?.message}</p>
-                     </div> */}
-                </div>
+            <div className='form-group'>
+                <label>Nome Completo</label>
+                <input 
+                type="text" 
+                id="name" 
+                {...register('name', {
+                    required: 'Este campo é obrigatório'
+                })}
+                placeholder="Digite seu nome completo" 
+                />
+                {errors.name && (
+                    <p className='text-xs text-red-500 mt-1'>
+                        {errors.name.message}
+                    </p>
+                )}
+            </div>
 
                 <div className='form-group'>
                     <label>CPF</label>
-                    <input type="text" id="cpf" ref={withMask('999.999.999-99')} />                
+                    <input type="text" id="cpf" {...register('cpf', { required: 'O CPF é obrigatório'})}
+                        ref={(e) => { if (e) {
+                        register('cpf').ref(e); 
+                        withMask(e, '999.999.999-99'); 
+                        }
+                        }} 
+                        placeholder="Digite seu CPF" 
+                    />
+                     {errors.cpf && ( 
+                        <p className='text-xs text-red-500 mt-1'>
+                        {errors.cpf.message}
+                        </p>
+                    )}               
                 </div>
 
                 <div className='form-group'>
-                    <label type="password">E-mail</label>
-                    <input type="email" id="email" placeholder="Digite seu e-mail" {...register('email')} />
+                    <label>E-mail</label>
+                    <input type="email" id="email" placeholder="Digite seu e-mail" {...register('email', {
+                            required: 'O e-mail é obrigatório',
+                            pattern: {
+                                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                message: 'Formato de e-mail inválido'
+                            }
+                        })} 
+                    />
+                    {errors.email && (
+                        <p className='text-xs text-red-500 mt-1'>
+                            {errors.email.message}
+                        </p>
+                    )}
                 </div>
 
                 <div className='input-field relative'>
-                    <label type="password">Senha</label>
+                    <label>Senha</label>
                     <input type={isPasswordVisible ? 'text' : 'password'} 
                     id="password" 
                     placeholder="Digite sua senha" 
                     {...register('password', {
-                        required: 'Sua senha deve conter no mínimo',
+                        required: 'Cria sua senha',
                         minLength: {
                             value: 6,
                             message: 'Sua senha deve conter no mínimo 8 caracteres, 1 letra maiúscula, 1 letra minúscula, 1 número, 1 caractere especial'
@@ -75,13 +103,13 @@ const RegisterTeacher = () => {
                     />
 
                     {errors.password && (
-                        <p className='mt-1 text-xs text-red-400'>
+                        <p className='mt-1 text-xs text-red-500'>
                             {errors.password?.message}
                         </p>
                     )}
 
                     <span className='absolute -right-3.5 top-10'>
-                        <button type='button' onClick={() => setIsPasswordVisible(!isPasswordVisible)}>
+                        <button type="button" onClick={() => setIsPasswordVisible(!isPasswordVisible)}>
                             {isPasswordVisible ? (
                                 <FaEye className="icon"/>
                                 
@@ -93,13 +121,25 @@ const RegisterTeacher = () => {
                     </span>
                 </div>
 
-                <div className='input-field relative pb-5'>
-                    <label >Confirmar Senha</label>
-                    <input type={isPasswordVisible ? 'text' : 'password'} id="password" placeholder="Digite sua senha"
-                        required
+                <div className='input-field relative'>
+                    <label>Confirmar Senha</label>
+                    <input 
+                        type={isPasswordVisible ? 'text' : 'password'} 
+                        id="confirmPassword" 
+                        placeholder="Confirme sua senha" 
+                        {...register('confirmPassword', {
+                            required: 'A confirmação de senha é obrigatória',
+                            validate: (value) =>
+                                value === getValues('password') || 'As senhas não coincidem'
+                        })} 
                     />
-                      <span className='absolute -right-3.5 top-10'>
-                        <button type='button' onClick={() => setIsPasswordVisible(!isPasswordVisible)}>
+                    {errors.confirmPassword && (
+                        <p className='text-xs text-red-500 mt-1'>
+                            {errors.confirmPassword.message}
+                        </p>
+                    )}
+                     <span className='absolute -right-3.5 top-10'>
+                        <button type="button" onClick={() => setIsPasswordVisible(!isPasswordVisible)}>
                             {isPasswordVisible ? (
                                 <FaEye className="icon"/>
                                 
@@ -110,7 +150,7 @@ const RegisterTeacher = () => {
                         </button>
                     </span>
                 </div>
-                <button type="submit" className="login-button">Finalizar Cadastro</button>
+                <button type="submit" className="login-button mt-3">Finalizar Cadastro</button>
 
             </form>
      </div>
